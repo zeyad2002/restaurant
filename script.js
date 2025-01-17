b420af356956ab44a95dcd23d2de08ff0857681b
Mohamedsayedddd <130872513+Mohamedsayedddd@users.noreply.github.com>
2025-01-17 03:55:34 +0200

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-
-const toggleButton = document.getElementById('toggle-nav');
-const navMenu = document.getElementById('nav-menu');
-
-
-toggleButton.addEventListener('click', () => {
-  navMenu.classList.toggle('show'); 
-});
-
